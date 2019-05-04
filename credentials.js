eeb87e67e837d5cd334205260b6b61ab1fac2693
shubham kakkar <shubham.kakkar98@gmail.com>
2019-05-05 00:59:06 +0530

    credentials

--- credentials.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoDBUrl: "mongodb+srv://shubhamkakkar:dl1cn2724@@nodejsapi-learn-rhsrv.mongodb.net/test?retryWrites=true",
-    client_id: "763458854890-0p40oh04eto9fbd8nmccu6b7pd7rksno.apps.googleusercontent.com", "project_id": "noble-trees-211819",
-    clientSecret: "P10Xq0o4lzQoamkX7R8A7-5o"
-}
